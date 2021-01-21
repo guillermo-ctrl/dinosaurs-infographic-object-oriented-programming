9ec3143da1955d9f7dca6f2cd3f53cb36c55c0ae
Your Name <you@example.com>
2021-01-21 10:08:42 +0100

    Creates the infographic template with images

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const dinoData = [
         "diet": "herbivor",
         "where": "North America",
         "when": "Late Cretaceous",
-        "fact": "First discovered in 1889 by Othniel Charles Marsh"
+        "fact": "Vision blocked by own horns.",
+        "extinct": true
     },
     {
         "species": "Tyrannosaurus Rex",
@@ -16,7 +17,8 @@ const dinoData = [
         "diet": "carnivor",
         "where": "North America",
         "when": "Late Cretaceous",
-        "fact": "The largest known skull measures in at 5 feet long."
+        "fact": "Can not actually run.",
+        "extinct": true
     },
     {
         "species": "Anklyosaurus",
@@ -25,7 +27,8 @@ const dinoData = [
         "diet": "herbivor",
         "where": "North America",
         "when": "Late Cretaceous",
-        "fact": "Anklyosaurus survived for approximately 135 million years."
+        "fact": "Anklyosaurus survived for approximately 135 million years.",
+        "extinct": true
     },
     {
         "species": "Brachiosaurus",
@@ -34,7 +37,8 @@ const dinoData = [
         "diet": "herbivor",
         "where": "North America",
         "when": "Late Jurasic",
-        "fact": "An asteroid was named 9954 Brachiosaurus in 1991."
+        "fact": "An asteroid was named 9954 Brachiosaurus in 1991.",
+        "extinct": true
     },
     {
         "species": "Stegosaurus",
@@ -43,7 +47,8 @@ const dinoData = [
         "diet": "herbivor",
         "where": "North America, Europe, Asia",
         "when": "Late Jurasic to Early Cretaceous",
-        "fact": "The Stegosaurus had between 17 and 22 seperate places and flat spines."
+        "fact": "The Stegosaurus had between 17 and 22 seperate places and flat spines.",
+        "extinct": true
     },
     {
         "species": "Elasmosaurus",
@@ -52,7 +57,8 @@ const dinoData = [
         "diet": "carnivor",
         "where": "North America",
         "when": "Late Cretaceous",
-        "fact": "Elasmosaurus was a marine reptile first discovered in Kansas."
+        "fact": "Elasmosaurus was a marine reptile first discovered in Kansas.",
+        "extinct": true
     },
     {
         "species": "Pteranodon",
@@ -61,7 +67,8 @@ const dinoData = [
         "diet": "carnivor",
         "where": "North America",
         "when": "Late Cretaceous",
-        "fact": "Actually a flying reptile, the Pteranodon is not a dinosaur."
+        "fact": "Actually a flying reptile, the Pteranodon is not a dinosaur.",
+        "extinct": true
     },
     {
         "species": "Pigeon",
@@ -70,7 +77,8 @@ const dinoData = [
         "diet": "herbivor",
         "where": "World Wide",
         "when": "Holocene",
-        "fact": "All birds are living dinosaurs."
+        "fact": "All birds are living dinosaurs.",
+        "extinct": false
     }
 ];
 // Create Dino Constructor
@@ -85,6 +93,7 @@ this.fact = fact;
 this.netWorth = 0;
 this.codingSkills = `The ${this.species} does not know how to code, hence it's net worth of ${this.netWorth} USD.`;
 this.opposableThumbs = 0;
+this.knowsHowToRead = false;
 }
 
 // Create Dino Objects
@@ -92,22 +101,125 @@ this.opposableThumbs = 0;
 //inheritance features. IMPROVEMENT: This seems like could be done by a foreach loop
 const triceratops = new dino(dinoData[0].species, dinoData[0].weight, dinoData[0].height, dinoData[0].diet, dinoData[0].where, dinoData[0].when, dinoData[0].fact)
 const tyrannosaurusRex = new dino(dinoData[1].species, dinoData[1].weight, dinoData[1].height, dinoData[1].diet, dinoData[1].where, dinoData[1].when, dinoData[1].fact)
-const anklyosaurus = new dino(dinoData[2].species, dinoData[2].weight, dinoData[0].height, dinoData[2].diet, dinoData[2].where, dinoData[2].when, dinoData[2].fact)
-const brachiosaurus = new dino(dinoData[3].species, dinoData[3].weight, dinoData[0].height, dinoData[3].diet, dinoData[3].where, dinoData[3].when, dinoData[3].fact)
-const stegosaurus = new dino(dinoData[4].species, dinoData[4].weight, dinoData[0].height, dinoData[4].diet, dinoData[4].where, dinoData[4].when, dinoData[4].fact)
-const lasmosaurus = new dino(dinoData[5].species, dinoData[5].weight, dinoData[0].height, dinoData[5].diet, dinoData[5].where, dinoData[5].when, dinoData[5].fact)
-const teranodon = new dino(dinoData[6].species, dinoData[6].weight, dinoData[0].height, dinoData[6].diet, dinoData[6].where, dinoData[6].when, dinoData[6].fact)
-const igeon = new dino(dinoData[7].species, dinoData[7].weight, dinoData[0].height, dinoData[7].diet, dinoData[7].where, dinoData[7].when, dinoData[7].fact)
+const anklyosaurus = new dino(dinoData[2].species, dinoData[2].weight, dinoData[2].height, dinoData[2].diet, dinoData[2].where, dinoData[2].when, dinoData[2].fact)
+const brachiosaurus = new dino(dinoData[3].species, dinoData[3].weight, dinoData[3].height, dinoData[3].diet, dinoData[3].where, dinoData[3].when, dinoData[3].fact)
+const stegosaurus = new dino(dinoData[4].species, dinoData[4].weight, dinoData[4].height, dinoData[4].diet, dinoData[4].where, dinoData[4].when, dinoData[4].fact)
+const lasmosaurus = new dino(dinoData[5].species, dinoData[5].weight, dinoData[5].height, dinoData[5].diet, dinoData[5].where, dinoData[5].when, dinoData[5].fact)
+const teranodon = new dino(dinoData[6].species, dinoData[6].weight, dinoData[6].height, dinoData[6].diet, dinoData[6].where, dinoData[6].when, dinoData[6].fact)
+const pigeon = new dino(dinoData[7].species, dinoData[7].weight, dinoData[7].height, dinoData[7].diet, dinoData[7].where, dinoData[7].when, dinoData[7].fact)
 
 // Create Human Object
 const human = {
   name: "",
-  height: 2,
+  heightFeet: 2,
+  heightInches: 2,
   weight: 2,
-  diet: ""
+  diet: "",
+  codingSkills: `Knows how to code.`,
+  opposableThumbs: 2,
+  knowsHowToRead: true,
 };
 
-// Use IIFE to get human data from form
+// Use IIFE to get human data (not an IIFE, don't know why IIFE)
+thename.addEventListener('input', function () {
+  human.name = event.target.value ;
+});
+diet.addEventListener('input', function () {
+  human.diet = event.target.value ;
+});
+feet.addEventListener('input', function () {
+  human.heightFeet = event.target.value ;
+});
+inches.addEventListener('input', function () {
+  human.heightInches = event.target.value ;
+});
+weight.addEventListener('input', function () {
+  human.weight = event.target.value ;
+});
+
+//This function can be used to remove elements by ID
+const removeelement = function (elementID) {
+  const element = document.getElementById(elementID);
+  element.remove()}
+
+//the following function creates the infographic when "submit" is clicked
+//first, the event listener to trigger the whole thing when "submit" is clicked
+btn.addEventListener('click', function () {
+  //immediately remove the form using the above function to remove elements
+  removeelement("dino-compare")
+  //put the grid and the grid items into a variable
+  const card = document.getElementsByClassName("grid-item")
+  const infographic = document.getElementById("grid");
+
+  //the following function creates the 9 grid elements (cards)
+  for (var x = 0; x < 9; x++) {
+    var newDiv = document.createElement('div'); // creates a new div element and assigns a variable to it
+    newDiv.classList.add('grid-item'); // gives the new div element the "grid item" variable
+    newDiv.setAttribute("id", x)
+    grid.appendChild(newDiv); //appends it to the parent container. This will repeat as many times as needed
+  }
+
+  //Arrays containing all images
+  const dinoImages = [];
+
+  //Adding each dinosaur image to the array:
+  const triceratopsImg = document.createElement('img');
+  triceratopsImg.setAttribute("src", "file:///C:/Users/GUILLE/Desktop/Documentos%20de%20Guille/lernen/JavaScript/assignment/Javascript/images/triceratops.png")
+  dinoImages.push(triceratopsImg)
+
+  const elasmosaurusImg = document.createElement('img');
+  elasmosaurusImg.setAttribute("src", "file:///C:/Users/GUILLE/Desktop/Documentos%20de%20Guille/lernen/JavaScript/assignment/Javascript/images/elasmosaurus.png")
+  dinoImages.push(elasmosaurusImg);
+
+  const anklyosaurusImg = document.createElement('img');
+  anklyosaurusImg.setAttribute("src", "file:///C:/Users/GUILLE/Desktop/Documentos%20de%20Guille/lernen/JavaScript/assignment/Javascript/images/anklyosaurus.png")
+  dinoImages.push(anklyosaurusImg);
+
+  const brachiosaurusImg = document.createElement('img');
+  brachiosaurusImg.setAttribute("src", "file:///C:/Users/GUILLE/Desktop/Documentos%20de%20Guille/lernen/JavaScript/assignment/Javascript/images/brachiosaurus.png")
+  dinoImages.push(brachiosaurusImg);
+
+  const pteranodonImg = document.createElement('img');
+  pteranodonImg.setAttribute("src", "file:///C:/Users/GUILLE/Desktop/Documentos%20de%20Guille/lernen/JavaScript/assignment/Javascript/images/pteranodon.png")
+  dinoImages.push(pteranodonImg);
+
+  const stegosaurusImg = document.createElement('img');
+  stegosaurusImg.setAttribute("src", "file:///C:/Users/GUILLE/Desktop/Documentos%20de%20Guille/lernen/JavaScript/assignment/Javascript/images/stegosaurus.png")
+  dinoImages.push(stegosaurusImg);
+
+  const tyrannosaurusRexImg = document.createElement('img');
+  tyrannosaurusRexImg.setAttribute("src", "file:///C:/Users/GUILLE/Desktop/Documentos%20de%20Guille/lernen/JavaScript/assignment/Javascript/images/tyrannosaurus%20rex.png")
+  dinoImages.push(tyrannosaurusRexImg);
+
+  //creating a variable for human and pigeon
+  const humanImage = document.createElement('img');
+  humanImage.setAttribute("src", "file:///C:/Users/GUILLE/Desktop/Documentos%20de%20Guille/lernen/JavaScript/assignment/Javascript/images/human.png")
+
+  const pigeonImage = document.createElement('img');
+  pigeonImage.setAttribute("src", "file:///C:/Users/GUILLE/Desktop/Documentos%20de%20Guille/lernen/JavaScript/assignment/Javascript/images/pigeon.png")
+
+  //the next function shuffles the array so that the order is different every time
+  function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+  };
+  //then we call the function to shuffle the array
+  shuffleArray(dinoImages);
+  //then add the human and pigeon to specific locations so that they always appear in the middle
+  dinoImages.splice(4, 0, humanImage);
+  dinoImages.splice(5, 0, pigeonImage);
+
+  //the next function appends an image to each card
+  dinoImages.forEach(function(item, index) {
+    card[index].appendChild(dinoImages[index]);
+  });
+
+  });
+
+
+
 
 
     // Create Dino Compare Method 1
@@ -125,9 +237,6 @@ const human = {
     // Generate Tiles for each Dino in Array
 
         // Add tiles to DOM
-
-    // Remove form from screen
-
 
 // On button click, prepare and display infographic
 
